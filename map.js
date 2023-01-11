b3cc295faad33f8d1d7ade8c8cb931f232612512
Rob Sizeland <robcakes69@gmail.com>
2023-01-11 10:36:01 +0000

    Map testing

--- map.js
+++ map.js
@@ -5,16 +5,27 @@
  */
 
 // Using a for loop
-
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for(let num of nums) {
+    results.push(num * 2);
+}
+console.log(results);
 
 // Using map()
-
+const multiByTwo = function(num) {
+    return num * 2;
+}
+const mapResults = nums.map(multiByTwo);
+console.log(mapResults);
 
 // Simplified w/ map()
-
+const mapSimple = nums.map(function(num) { return num * 2 });
+console.log(mapSimple);
 
 // Simplfied w/ map() + arrow function
-
+const mapEvenSimpler = nums.map(n => n * 2);
+console.log(mapEvenSimpler);
 
 // With objects:
 const students = [
@@ -37,3 +48,6 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentWithIds = students.map(student => [student.name, student.id]);
+console.log(studentWithIds);
